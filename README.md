289415020141c589be0954c37668f3c52b0608dc
Chris Matheson <chris@matheson.it>
2017-09-05 14:45:13 +0100

    added route for paraterized cal creation

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # lily-cal
+
+#RESEARCH
+
+one of the goals will be "full stack" rust. To this end i think that a string based templating engine is a bit of a cop out. So some options for using "native" rust to create HTML output are ....
+
+https://docs.rs/domafic/0.2.0/domafic/#
+
+String absed but might give the compile time safety required
+
+https://libraries.io/cargo/borealis
+https://libraries.io/cargo/bart
--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,57 @@
 extern crate rocket;
 extern crate icalendar;
 extern crate chrono;
+extern crate domafic;
+use domafic::tags::{div, h1, p};
+use std::marker::PhantomData;
+use chrono::prelude::*;
+use icalendar::{Component, Calendar, Event, Property};
+use rocket::response::{Failure};
+use rocket::http::Status;
 use std::ops::Add;
-use chrono::{Utc, TimeZone};
-use icalendar::{Component, Calendar, Event};
+
+type Msg = ();
 
 #[get("/")]
-fn hello() -> &'static str {
-    return "Hello, world!"
+fn index() -> String {
+    return div((
+        h1("Welcome to Baby-Cal"),
+        p("This page will create you a calendar with usefull at-a-glance info about your babies age."),
+        PhantomData::<Msg>
+    )).to_string();
 }
 
+
 #[get("/lily")]
-fn lily() -> String {
-    return gen_cal().to_string()
+fn lily() -> Result<String, Failure> {
+    return cal("Lily".to_owned(), "2017-04-12T00:00:00+00:00".to_owned());
+}
+
+#[get("/cal/<name>/<dob>")]
+fn cal(name: String, dob: String) -> Result<String, Failure> {
+    return match DateTime::parse_from_rfc3339(dob.as_str()) {
+        Ok(date)  => Ok(build_cal(name, date.date()).to_string()),
+        _ => Err(Failure(Status::raw(400))),
+    };
+}
+
+fn build_cal(name: String, dob: Date<FixedOffset>) -> Calendar {
+    let mut calendar = Calendar::new();
+
+    let mut busy = Property::new("TRANSP", "TRANSPARENT");
+
+    for x in 0..52 {
+        calendar.push(Event::new()
+            .all_day(dob.add(chrono::Duration::weeks(x)))
+            .append_property(busy.done())
+            .summary(&format!("{} is {} weeks old", name, x))
+            .done());
+    }
+    return calendar
 }
 
 fn main() {
     rocket::ignite().mount("/",
-        routes![hello, lily]
+        routes![index, lily, cal]
     ).launch();
-}
-
-fn gen_cal() -> icalendar::Calendar {
-    let mut calendar = Calendar::new();
-
-    for x in 0..52 {
-        calendar.push(Event::new()
-            .all_day(Utc.ymd(2017, 4, 12).add(chrono::Duration::weeks(x)))
-            .summary(&format!("Lily is {} weeks old", x))
-            .done());
-    }
-
-    return calendar;
 }
